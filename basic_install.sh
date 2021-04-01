6755edd74cf7369a2427ec9662e0b8be616aa4fb
Nevertheless <swgu931@chol.com>
2021-04-01 11:37:58 +0900

    ubuntu basic install packages

---
+++ basic_install.sh
@@ -0,0 +1,8 @@
+# basic install program 
+- Something after linux installed
+
+sudo apt-get update -y
+sudo apt-get install -y net-tools iputils-ping openssh-server ca-certificates 
+
+
+sudo dpkg-reconfigure ca-certificates 
