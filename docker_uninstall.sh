65845400e3d5b10bf27f721d07555033a3d52062
Nevertheless <swgu931@chol.com>
2021-05-11 14:19:16 +0900

    Update docker_uninstall.sh

--- docker_uninstall.sh
+++ docker_uninstall.sh
@@ -1,15 +1,16 @@
 # docker uninstall
 #!/bin/bash
 
-sudo apt-get purge docker-ce
-sudo apt-get autoremove --purge docker-engine
+sudo apt-get purge -y docker-ce
+sudo apt-get autoremove --purge -y docker-engine
 sudo rm -rf /var/lib/docker
 sudo groupdel docker
 sudo rm -rf /var/run/docker.sock
 sudo rm -rf /etc/docker
 sudo rm -rf /usr/bin/docker
+sudo rm -rf /etc/systemd/system/docker.service.d
 
-sudo apt-get purge docker-engine
+sudo apt-get purge -y docker-engine
 sudo apt-get purge -y docker-engine docker docker.io docker-ce  
 sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce  
 
