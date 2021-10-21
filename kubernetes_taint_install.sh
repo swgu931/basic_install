3252ae5fef5b543940022470db4ecc87ba9a7a03
sewan.gu <sewan.gu@lge.com>
2021-10-21 14:48:18 +0900

    Add git_basics.txt and so on

--- kubernetes_taint_install.sh
+++ kubernetes_taint_install.sh
@@ -68,4 +68,5 @@ echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
 
+source ~/.bashrc
 echo "Successfully done"
