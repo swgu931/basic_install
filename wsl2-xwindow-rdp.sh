2efeef769a817cfa0c7a6c73b685f4bb1325f916
Nevertheless <swgu931@chol.com>
2021-12-16 23:08:16 +0900

    Modify wsl2-xwindow-rdp.sh for directory

--- wsl2-xwindow-rdp.sh
+++ wsl2-xwindow-rdp.sh
@@ -11,6 +11,6 @@ sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
 sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
 sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
 
-sudo echo "startxfce4" >> startwm.sh
+sudo echo "startxfce4" >> /etc/xrdp/startwm.sh
 sudo /etc/init.d/xrdp start
 
