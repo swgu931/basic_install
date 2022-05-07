7e0f8e98830cfe3f986ad8b1bb2810bf3b4ac858
sewan.gu <sewan.gu@lge.com>
2022-05-07 12:09:52 +0900

    Add helm install

--- helm_install.sh
+++ helm_install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 
-#curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-#chmod 700 get_helm.sh
-#./get_helm.sh
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+#curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+
+helm plugin install https://github.com/databus23/helm-diff
+helm plugin install https://github.com/jkroepke/helm-secrets --version v3.12.0
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
