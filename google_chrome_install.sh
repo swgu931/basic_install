6bc799e72b968c8d61780d7511bd0b75c0655113
Nevertheless <swgu931@chol.com>
2022-09-27 16:02:08 +0900

    Modify chrome install shell

--- google_chrome_install.sh
+++ google_chrome_install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
-sudo apt-get update -y 
-sudo apt-get install -y google-chrome-stable
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update -y
+sudo apt install google-chrome-stable -y
+google-chrome --version
+
+
 #sudo rm -rf /etc/apt/sources.list.d/google.list
 #sudo rm -rf /etc/apt/sources.list.d/google-chrome.list
