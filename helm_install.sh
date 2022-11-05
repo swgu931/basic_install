dbf75fb4f866339e84edba3b84852aa5f80b9a45
Nevertheless <swgu931@chol.com>
2022-11-05 16:46:39 +0900

    Modify helm_install.sh

--- helm_install.sh
+++ helm_install.sh
@@ -8,7 +8,37 @@ chmod 700 get_helm.sh
 #curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
 
-helm plugin install https://github.com/databus23/helm-diff
-helm plugin install https://github.com/jkroepke/helm-secrets --version v3.12.0
+#helm plugin install https://github.com/databus23/helm-diff
+#helm plugin install https://github.com/jkroepke/helm-secrets --version v3.12.0
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
+
+# Repository add/remove
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm repo list
+
+# Repository chart search
+#helm search repo
+#helm search repo mariadb
+
+# chart info 
+#helm show chart bitnami/mariadb
+#helm inspect values bitnami/mariadb
+
+# chart archive install
+#helm install add-db bitnami/mariadb
+#helm install app-db --set auth.rootPassword=secretpassword,auth.database=app_database bitnami/mariadb
+#helm list
+
+# chart archive uninstall
+#helm uninstall add-db
+
+
+# exercise
+
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm repo list
+#helm search repo bitnami/nginx
+#helm install ckad-webserver bitnami/nginx
+#helm list
+#kubectl get all
+#helm uninstall ckad-webserver
