9bfd21dc01cbc803826a54ade1277e8893b7d65d
swgu931 <swgu931@naver.com>
2025-04-22 09:11:01 +0900

    Modify citrix_install.h

--- citrix_install.sh
+++ citrix_install.sh
@@ -2,6 +2,7 @@
 
 
 # download from https://www.citrix.com/downloads/workspace-app/linux/workspace-app-for-linux-latest.html
+# 2207 : https://www.citrix.com/downloads/workspace-app/legacy-workspace-app-for-linux/workspace-app-for-linux-latest-OLD1.html?srsltid=AfmBOopqdAclPQYvWL0d8OWWBz1ioTWohU-RSCAg_QGx0Ewh6SgjvbtR
 # manual from https://docs.citrix.com/en-us/citrix-workspace-app-for-linux/install.html#debian-packages
 
 # ssl issue ref: https://askubuntu.com/questions/1228614/installing-citrix-receiver-on-ubuntu-20-04-missing-libwebkitgtk-1-0-0
