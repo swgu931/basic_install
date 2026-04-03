ee5f91adc46a359045c5042b66026590399112c4
swgu931 <swgu931@naver.com>
2026-04-03 15:25:15 +0900

    Add ros2 gpg key update shell

---
+++ ros2-gpg-key-update.sh
@@ -0,0 +1,6 @@
+# 1. 최신 ROS 2 GPG 키를 다운로드하여 키링 업데이트
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+# 2. apt 리스트 다시 업데이트
+sudo apt update
+
