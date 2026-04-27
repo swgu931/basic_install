ae5ee52479a80b243e7e984580d0bff7a2192256
swgu931 <swgu931@naver.com>
2026-04-27 16:05:23 +0900

    Add mermaid-install.sh

---
+++ mermaid-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo snap install mermaid-cli
+
+## howto use (output format: png, svg, pdf) (scale : resolution, density)
+# mmdc -i input.mmd -o output.png --scale 3
+
--- ros2-jazzy-gazebo-rviz2-install-exec.sh
+++ ros2-jazzy-gazebo-rviz2-install-exec.sh
@@ -5,7 +5,13 @@ sudo apt install ros-jazzy-ros-gz
 sudo apt install ros-jazzy-ros-gz-bridge 
 sudo apt install ros-jazzy-gz-ros2-control
 sudo apt install ros-jazzy-turtlebot4-navigation
-sudo apt install ros-jazzy-turtlebot4-gz-bringup 
+sudo apt install ros-jazzy-turtlebot4-gz-bringup
+sudo apt install ros-jazzy-turtlebot3-teleop
+
+sudo apt install ros-jazzy-turtlebot4-bringup
+sudo apt install ros-jazzy-turtlebot4-robot
+sudo apt install ros-jazzy-turtlebot4-simulator
+sudo apt install ros-jazzy-turtlebot4-desktop
 
 
 # gz sim
